818115a785ac0a81d6a3f821f6eb53166166c4a1
Amy Wang <why812@gmail.com>
2013-07-16 01:51:53 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+
+var data=fs.readFileSync("index.html", "utf8");
+console.log(data);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
